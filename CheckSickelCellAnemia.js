0c13880044bbc3ead1c83af4b19d799261b8d8a0
marmik pandya <marmikpandya@gmail.com>
2015-02-01 17:32:48 +0530

    Checks for Sickel Cell Anemia as well Bitches!

---
+++ CheckSickelCellAnemia.js
@@ -0,0 +1,35 @@
+var sca = require('./genoset-sickel-cell-anemia');
+var fs = require('fs');
+var path = require('path');
+var es = require('event-stream');
+var JSONStream = require('JSONStream');
+
+var jsonStream = fs.createReadStream(path.join(__dirname, "./data/23andme-male.json"));
+
+var query = sca();
+var genoStream = query.stream();
+
+jsonStream
+  .pipe(JSONStream.parse('*'))
+  .pipe(genoStream);
+
+var count = 0;
+genoStream.on('data', function(snp) {
+  console.log('Analyzed', ++count, 'SNPs');
+});
+
+genoStream.on('end', function() {
+  console.log("There are", query.matches().length, "matches for genoset 228");
+  console.log("There is a", query.percentage(), "percent chance that genoset matches");
+  if (query.percentage() == 0){
+
+  	console.log("No Sickel Cell Anemia BITCH!! ");
+  }
+
+  else {
+
+  	console.log("I'm sorry bro, You've got Sickel Cell Anemia ");
+
+  }
+});
+
